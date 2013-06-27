7609ef1eb16a8191185580ff9e79fab6eddfb038
Lukas Hohnsel <lukashohnsel@Lukass-MacBook-Pro.local>
2013-06-27 14:08:41 +0200

    Finish user edit, update, index and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,9 +13,9 @@ require 'spec_helper'
 
 describe User do
 
-	it { should respond_to(:password_confirmation) }
-  	it { should respond_to(:remember_token) }
-  	it { should respond_to(:authenticate) }
+	   it { should respond_to(:password_confirmation) }
+  	 it { should respond_to(:remember_token) }
+  	 it { should respond_to(:authenticate) }
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com", 
@@ -102,5 +102,19 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
 end
+ it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 end
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -56,4 +56,16 @@ describe "User pages" do
       end
     end
   end
+
+   
+     
+
+    
+     describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+  end
 end
