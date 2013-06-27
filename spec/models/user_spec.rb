4b53feb6d028c7f6a6537a10510b4db7740939db
Lukas Hohnsel <lukashohnsel@Lukass-MacBook-Pro.local>
2013-06-27 17:41:25 +0200

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,14 @@ describe User do
   	  it { should respond_to(:authenticate) }
       it { should respond_to(:microposts) }
       it { should respond_to(:feed) }
+      it { should respond_to(:feed) }
+      it { should respond_to(:relationships) }
+      it { should respond_to(:followed_users) }
+      it { should respond_to(:reverse_relationships) }
+      it { should respond_to(:followers) }
+      it { should respond_to(:following?) }
+      it { should respond_to(:follow!) }
+      it { should respond_to(:unfollow!) }
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com", 
@@ -148,7 +156,35 @@ end
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+ 
+      let(:followed_user) { FactoryGirl.create(:user) }
 
-      its(:feed) { should_not include(unfollowed_post) }
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+      
     end
+   
+
+    describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+
+    describe "and unfollowing" do
+    before { @user.unfollow!(other_user) }
+
+      it { should be_following(other_user) }
+      its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+    end
+    end
+  end
   end
